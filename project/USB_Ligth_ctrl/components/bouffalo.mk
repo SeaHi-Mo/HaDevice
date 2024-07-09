1b96650f7310f11dfa141b337a686a3693f2596c
SeaHi-Mo <seahi@foxmail.com>
2024-07-09 18:11:32 +0800

    完成配网+连接+控制

--- project/USB_Ligth_ctrl/components/bouffalo.mk
+++ project/USB_Ligth_ctrl/components/bouffalo.mk
@@ -1,7 +1,9 @@
-COMPONENT_ADD_INCLUDEDIRS := ../../HomeAssistant/HomeAssistant-C
+include $(BL60X_SDK_PATH)/components/network/ble/ble_common.mk
 
+COMPONENT_ADD_INCLUDEDIRS += ../../../HomeAssistant/HomeAssistant-C
+COMPONENT_ADD_INCLUDEDIRS += wifi flash device_state homeAssistant
 
-COMPONENT_SRCDIRS := ../../HomeAssistant/HomeAssistant-C
-
+COMPONENT_SRCDIRS += ../../../HomeAssistant/HomeAssistant-C
+COMPONENT_SRCDIRS += wifi flash device_state homeAssistant
 
 COMPONENT_OBJS := $(patsubst %.c,%.o, $(COMPONENT_SRCS))
--- project/USB_Ligth_ctrl/main/main.c
+++ project/USB_Ligth_ctrl/main/main.c
@@ -15,17 +15,29 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
-
+#include "blog.h"
 #include <lwip/tcpip.h>
-
+#include <easyflash.h>
 #include <bl602_glb.h>
-
 #include <bl_sys.h>
-
+#include "device_state.h"
 
 void main()
 {
     bl_sys_init();
+
+    easyflash_init();
+
+    device_state_init(NULL);
+    blog_info("[OS] Starting TCP/IP Stack...");
     tcpip_init(NULL, NULL);
-    printf("blufi demo test\r\n");
+    blog_info("[OS] proc_main_entry task...");
+
+
+    while (1)
+    {
+        blog_debug("HeapSize=%d ", xPortGetFreeHeapSize());
+        vTaskDelay(pdMS_TO_TICKS(3000));
+
+    }
 }
